32eeae23c608441c6953c2bdb47b4e30783e9f6c
Pumelowsky <pumelowsky@gmail.com>
2022-05-24 21:20:13 +0200

    Repaired typing animation

--- js/text.js
+++ js/text.js
@@ -1,25 +1,25 @@
 var my_titles = ['Programmer', 'IT technician', 'Tester'];
 var textPos = 0;
 var speed = 100;
-var i = 0;
+var index = 0;
 
 typedewriter = () => {
-    document.querySelector("#changing-text").innerHTML = my_titles[i].substring(0, textPos) + "<span id='blinker'>\u25ae</span>";
+    document.querySelector("#changing-text").innerHTML = my_titles[index].substring(0, textPos);
     if(textPos-- != 0)
         setTimeout(typedewriter, speed);
     else
     {
-        if(i == my_titles.length-1)
-            i=0;
+        if(index == my_titles.length-1)
+            index=0;
         else 
-            i++;
+            index++;
         typewriter();
     }
 }
 
 typewriter = () => {
-    document.querySelector("#changing-text").innerHTML = my_titles[i].substring(0, textPos) + "<span id='blinker'>\u25ae</span>";
-    if(textPos++ != my_titles[0].length)
+    document.querySelector("#changing-text").innerHTML = my_titles[index].substring(0, textPos);
+    if(textPos++ != my_titles[index].length)
         setTimeout(typewriter, speed);
     else
         typedewriter();
